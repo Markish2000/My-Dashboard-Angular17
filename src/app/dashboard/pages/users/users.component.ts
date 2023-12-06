ab47e74fc37a2a8821c5d72c6f34c9f020b031ca
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-12-05 22:52:54 -0300

    drag y drop

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// Libraries
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+
 // Components
 import { TitleComponent } from '@shared/title/title.component';
 
@@ -11,9 +18,15 @@ import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, TitleComponent, RouterModule],
+  imports: [CommonModule, DragDropModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
 })
 export default class UsersComponent {
   public usersService = inject(UsersService);
+
+  public onDropped(event: CdkDragDrop<any>): void {
+    const previous = event.previousIndex;
+    const current = event.currentIndex;
+    // moveItemInArray(this.usersService, previous, current);
+  }
 }
